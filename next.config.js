f8a8761307722b9c18bb6a3572472faa4e7c19fd
Pedro <pedro.silva@ringa.com.br>
2023-08-23 16:03:39 -0300

    changes config

--- next.config.js
+++ next.config.js
@@ -2,9 +2,6 @@ const { hostname } = require("os");
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    serverComponentsExternalPackages: ["@acme/ui"],
-  },
   images: {
     domains: ["res.cloudinary.com"],
   },
