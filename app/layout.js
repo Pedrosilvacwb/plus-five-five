ac31789c72d527c95151de45dc970a4540c7feb6
Pedro <pedro.silva@ringa.com.br>
2024-01-17 16:32:37 -0300

    Fix typescript

---
+++ app/layout.js
@@ -0,0 +1,16 @@
+"use strict";
+exports.__esModule = true;
+var google_1 = require("next/font/google");
+require("./globals.css");
+var inter = google_1.Inter({ subsets: ["latin"] });
+exports.metadata = {
+    title: "+55 Coffe Shop",
+    description: "High quality Brazilian Coffee"
+};
+function RootLayout(_a) {
+    var children = _a.children;
+    return (<html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>);
+}
+exports["default"] = RootLayout;
---
+++ app/page.js
@@ -0,0 +1,11 @@
+"use strict";
+exports.__esModule = true;
+var HeroSection_1 = require("@/components/Home/HeroSection");
+var Overlay_1 = require("@/components/Home/Overlay");
+var page = function () {
+    return (<div className="w-full  max-w-[100vw] overflow-x-hidden ">
+      <HeroSection_1["default"] />
+      <Overlay_1["default"] />
+    </div>);
+};
+exports["default"] = page;
